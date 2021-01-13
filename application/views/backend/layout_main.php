e869f077b939952416558a21becb235a7d66ac76
Avinash Gunjkar <avinash.gunjkar@gmail.com>
2021-01-13 10:30:03 +0000

    this is delete

--- application/views/backend/layout_main.php
+++
@@ -1,7 +0,0 @@
-<?php
-    defined('BASEPATH') OR exit('No direct script access allowed');
-    $this->load->view('backend/components/header');
-    $this->load->view('backend/components/sidebar');
-    $this->load->view($page);
-    $this->load->view('backend/components/footer');
-?>
