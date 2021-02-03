ceee1a69acc0c87ee60175e0bd41a241c72c435c
avinash-gunjkar <54440753+avinash-gunjkar@users.noreply.github.com>
2021-02-03 16:57:56 +0530

    Update Home.php

--- newweb/app/Controllers/Home.php
+++ newweb/app/Controllers/Home.php
@@ -16,25 +16,25 @@ class Home extends BaseController
 	{
 		//throw new Exception("failed");
 		//$this->load->database();
-		$db = \Config\Database::connect('default');
-		$q=$db->query("select * from cust");
-		echo "Database connected sucessfully";
+		//$db = \Config\Database::connect('default');
+		//$q=$db->query("select * from cust");
+		//echo "Database connected sucessfully";
 		//echo $db->query("SELECT VERSION()")->row('version');
-		//return view('welcome_message');
+		return view('welcome_message');
 		
 	}
 	public function test(){
 		//$constr=("host=localhost port=5432 dbname=test user=test password=test");
-		$con=\Config\Database::connect('default');
+		//$con=\Config\Database::connect('default');
 		//	$this->load->batabase();
-	for ($i=1;$i<=1;$i++){
-	$pg_sql="INSERT INTO cust(id,name,address,phone,state)VALUES(1,'avinash sharadrao gunjkar','pune maher
-astra khese park',1818,'Maharastra')";
-$result=$con->query($pg_sql);
-	}
- 	echo "Record Inserted";	
+	//for ($i=1;$i<=1;$i++){
+	//$pg_sql="INSERT INTO cust(id,name,address,phone,state)VALUES(1,'avinash sharadrao gunjkar','pune maher
+//astra khese park',1818,'Maharastra')";
+//$result=$con->query($pg_sql);
+//	}
+ //	echo "Record Inserted";	
 
-	}
+//	}
 	//--------------------------------------------------------------------
 
 }
